0ee3bbae34d9bbefce2fd809417de74d82cda800
Begaim Akhmatova <begaimakhmatova91@gmail.com>
2021-11-28 15:34:45 +0600

    toggle paragraph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React,{useState}  from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => {
+  const [showParagraph, setShowParagaph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagaph((prevShowParagraph) => !prevShowParagraph)
+  }
+
+  return(
+    <div>
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is the new</p>}
+      <button onClick={toggleParagraphHandler}>Toggle paragraph</button>
+    </div>
+  )
+}
 export default App;
